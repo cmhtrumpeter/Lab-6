f4b1d25c72285781514fe0bd04a7d76887f1cef3
Ben Davidson <BenDavidson@Brandons-MacBook-Pro.local>
2024-03-20 14:06:04 -0400

    added decode function

--- lab6.py
+++ lab6.py
@@ -12,8 +12,25 @@ def encode(password):
     return encoded
 
 
+# Ben Davidson - added decode function
 def decode(encoded):
-    pass
+    temp_list = [int(i) for i in encoded]
+
+    for index in range(len(temp_list)):
+        if temp_list[index] == 2:
+            temp_list[index] = 9
+        elif temp_list[index] == 1:
+            temp_list[index] = 8
+        elif temp_list[index] == 0:
+            temp_list[index] = 7
+        else:
+            temp_list[index] -= 3
+
+    temp_list = [str(i) for i in temp_list]
+    decoded_password = ''.join(temp_list)
+
+    return decoded_password
+
 
 def main():
     while True:
@@ -25,12 +42,15 @@ def main():
         menu_opt = int(input("Please enter an option: "))
         if menu_opt == 1:
             password = input("Please enter your password to encode: ")
-            encode(password)
+            encoded_password = encode(password)
             print("Your password has been encoded and stored!\n")
         elif menu_opt == 2:
-            pass
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
         elif menu_opt == 3:
-            pass
+            break
+        else:
+            print("Incorrect choice, please try again.\n")
 
 
 if __name__ == "__main__":
